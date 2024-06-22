fa78d30a94e04c7f6eeb99ba576e1ba866f370ff
Fahri Fachrezzy <fahrifachrezzy89@gmail.com>
2024-06-22 18:13:54 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function khodamAnda() {
+  const name = document.getElementById("inputNama").value;
+
+  if (!name) {
+    const warning = document.getElementById("warning");
+    warning.append("!Masukan Nama Anda");
+    return;
+  }
+
+  const popup = document.getElementById("popup");
+  popup.classList.add("popup");
+
+  const hasil = document.getElementById("khodam");
+  const khodams = [
+    "Tidak Memiliki khodam",
+    "Ambatukam",
+    "Si imut",
+    "Mas Rusdi",
+    "Ambatron",
+    "Ambacong",
+  ];
+  const randomKhodams = Math.floor(Math.random() * khodams.length);
+  const hasilTeks = khodams[randomKhodams];
+  hasil.append(hasilTeks);
+}
+
+function back() {
+  window.location.reload();
+}
